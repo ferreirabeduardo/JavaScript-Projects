7f1e2f870aa10a1c3c8b5b46407b3977f26d7a16
ferreved <ferreved@gmail.com>
2026-09-02 12:01:09 +0100

    Add Basic JavaScript Projects - Project1 expressions and alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+// Show a popup alert when the page loads
+window.alert("Welcome to my first JavaScript project!");
+
+// Create two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate (join) the two variables together
+var fullSentence = Sent1 + Sent2;
+
+// Write the joined sentence onto the page
+document.write(fullSentence);
+
+// Write an expression (a calculation that produces a value)
+var total = 5 + 10;
+
+// Display the result of the expression on the page
+document.write("<br>The expression 5 + 10 equals: " + total);
